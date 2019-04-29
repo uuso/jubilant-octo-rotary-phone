b78b12996b772b58a4d5c97222670d4edc07b866
uuso <47225119+uuso@users.noreply.github.com>
2019-04-29 16:41:31 +0300

    Update settings.py

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -133,8 +133,8 @@ STATICFILES_DIRS = [
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-EMAIL_HOST = os.environ.get("EMAIL_HOST")
-EMAIL_PORT = int(os.environ.get("EMAIL_PORT"))
+# EMAIL_HOST = os.environ.get("EMAIL_HOST")
+# EMAIL_PORT = int(os.environ.get("EMAIL_PORT"))
 
 
 
